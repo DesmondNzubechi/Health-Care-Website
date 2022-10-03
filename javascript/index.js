41104514cef33c2631b300ab937a64dbc1b94d0b
DesmondNzubechi <nzubestdesmond@gmail.com>
2022-10-03 14:24:16 +0100

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,80 @@
+let open = document.querySelector('#open');
+let close = document.querySelector('#close');
+let navLinks = document.querySelector('.links');
+let dropdownLinks = document.querySelectorAll('.dropdownLinks');
+let dropdown = document.querySelectorAll('.dropdown');
+
+
+open.addEventListener('click', (e) => {
+console.log(e.target.parentElement)
+
+navLinks.style.left = '0';
+open.style.display = 'none';
+close.style.display = 'block';
+
+});
+
+close.addEventListener('click', (e) => {
+console.log(e.target.parentElement)
+
+navLinks.style.left = '-1000px';
+open.style.display = 'block';
+close.style.display = 'none';
+
+});
+
+
+let act = document.querySelector('.acct');
+let ac =  document.querySelector('.ac');
+act.addEventListener('mouseover', () => {
+	let user = document.querySelector('.users');
+	user.style.display = 'block';
+});
+act.addEventListener('mouseout', () => {
+	let user = document.querySelector('.users');
+	user.style.display = 'none';
+
+});
+
+let drop = document.querySelector('.drop');
+
+drop.addEventListener('mouseover', () => {
+	let drp = document.querySelector('.drp');
+	drp.style.display = 'block';
+	let down = document.querySelector('#down');
+	down.style.display = 'none';
+	let up = document.querySelector('#up');
+	up.style.display = 'inline-flex';
+
+});
+drop.addEventListener('mouseout', () => {
+	let drp = document.querySelector('.drp');
+	drp.style.display = 'none';
+	let down = document.querySelector('#down');
+	down.style.display = 'inline-flex';
+	let up = document.querySelector('#up');
+	up.style.display = 'none';
+
+});
+
+let abt = document.querySelector('.abt')
+abt.addEventListener('mouseover', () => {
+	let sub = document.querySelector('.sub');
+	sub.style.display = 'block';
+	let down = document.querySelector('#downn');
+	down.style.display = 'none';
+	let up = document.querySelector('#upp');
+	up.style.display = 'inline-flex';
+
+});
+abt.addEventListener('mouseout', () => {
+	let sub = document.querySelector('.sub');
+	sub.style.display = 'none';
+	let down = document.querySelector('#downn');
+	down.style.display = 'inline-flex';
+	let up = document.querySelector('#upp');
+	up.style.display = 'none';
+
+})
+
+
